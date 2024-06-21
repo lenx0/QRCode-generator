337d780c7d0d8c211e4ebb817f86054e353a414e
thiago <thiagofreitas1106@gmail.com>
2024-06-21 13:34:13 -0300

    footer: done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import QRCode from "./QRCode"
 import "./App.css"
+import Footer from "./Components/Footer"
 
 function App() {
   return (
+    <>
     <QRCode />
+    <Footer />
+    </>
   )
 }
 
---
+++ src/Components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div style={{ width: "100vw", textAlign: "center", position: "absolute", bottom: 0}}>
+            <h6>Desenvolvido por Thiago Beraldo</h6>
+        </div>
+    )
+}
+
+export default Footer
